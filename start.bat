7fcf2de9cfcd705ae92af379aecb169e31ea96c6
Evan <87098365+YeahMan406@users.noreply.github.com>
2022-06-23 07:24:14 +0100

    lol

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-cd /d C:\NoPixel\cfx-server-data
-C:\NoPixel\[artifact]\FXServer.exe +exec server.cfg +exec resources.cfg +set gamename gta +set sv_enforceGameBuild 1436 +set onesync on
+cd /d C:\dares-fivem-leaks\cfx-server-data
+C:\dares-fivem-leaks\[artifact]\FXServer.exe +exec server.cfg +exec resources.cfg +set gamename gta +set sv_enforceGameBuild 1436 +set onesync on
