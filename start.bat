1d3c2910301a200a1fd3230d71f8f450dbd409a5
Evan <87098365+YeahMan406@users.noreply.github.com>
2022-06-26 19:28:03 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-cd /d D:\Localhosts\dares-fivem-leaks\cfx-server-data
-D:\Localhosts\dares-fivem-leaks\[artifact]\FXServer.exe +exec server.cfg +exec resources.cfg +set gamename gta +set sv_enforceGameBuild 1436 +set onesync on
+cd /d C:\dares-fivem-leaks\cfx-server-data
+C:\dares-fivem-leaks\[artifact]\FXServer.exe +exec server.cfg +exec resources.cfg +set gamename gta +set sv_enforceGameBuild 1436 +set onesync on
