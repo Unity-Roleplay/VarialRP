4b60c2fd8877fe095ce2224d2b036bf0a1c14de2
Hxper <prodbyhyper@gmail.com>
2022-07-15 12:31:01 -0700

    Phone stuff

--- start.bat
+++
@@ -1,2 +0,0 @@
-cd /d C:\dares-fivem-leaks\cfx-server-data
-C:\dares-fivem-leaks\[artifact]\FXServer.exe +exec server.cfg +exec resources.cfg +set gamename gta +set sv_enforceGameBuild 1436 +set onesync on
