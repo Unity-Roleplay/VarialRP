1bb76e97548f997eaf9df6031b4eef485155d167
Hxper <prodbyhyper@gmail.com>
2022-07-15 11:52:02 -0700

    Revert "Merge branch 'main' of https://github.com/Unity-Roleplay/VarialRP"
    This reverts commit dd2292a76062c06560ca958c6d31040afd6e0d94, reversing
    changes made to 50262e08c5a91d950b9505d499799f7665d3a7b5.

---
+++ start.bat
@@ -0,0 +1,2 @@
+cd /d C:\dares-fivem-leaks\cfx-server-data
+C:\dares-fivem-leaks\[artifact]\FXServer.exe +exec server.cfg +exec resources.cfg +set gamename gta +set sv_enforceGameBuild 1436 +set onesync on
